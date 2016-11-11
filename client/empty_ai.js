4d55a009c0fea7a94e6348ad1baab9f2a81d6ca6
Nicolas Hurtubise <316k@legtux.org>
2016-11-11 11:52:59 -0500

    Adds documentation in empty_ai.js

--- client/empty_ai.js
+++ client/empty_ai.js
@@ -1,31 +1,38 @@
+/**
+ * Fonction utilisée *à l'interne* par votre code.
+ * Vous devriez passer par cette fonction pour mettre à jour
+ * à la fois votre modèle interne de grille et votre interface.
+ */
 function setGrid(x, y, val) {
     // TODO : colorier la grille de la bonne couleur
     // TODO : mettre à jour le modèle
 }
 
-function createGrid(config) {
-    /*
-       config est un enregistrement javascript de la forme :
-       {
-          w: longueur de la grille
-          h: hauteur de la grille
-          obstacles: tableau de deux enregistrements représentant des obstacles, de la forme :
-            {
-               x: position horizontale de l'obstacle
-               y: position verticale de l'obstacle
-               w: largeur de l'obstacle
-               h: hauteur de l'obstacle
-           }
-          players: tableau de 2 enregistrements représentant des joueurs, de la forme :
-           {
-              x: position de départ en x du joueur
-              y: position de départ en y du joueur
-              id: numéro d'identification du joueur
-              direction: direction initiale du joueur
-           }
-         me: numéro d'identification (id) de votre joueur
+/**
+ * Fonction appelée lorsque le jeu commence. La fonction reçoit
+   la configuration initiale du jeu dans le paramètre config.
+   config est un enregistrement javascript de la forme :
+   {
+      w: longueur de la grille
+      h: hauteur de la grille
+      obstacles: tableau de deux enregistrements représentant des obstacles, de la forme :
+        {
+           x: position horizontale de l'obstacle
+           y: position verticale de l'obstacle
+           w: largeur de l'obstacle
+           h: hauteur de l'obstacle
        }
-     */
+      players: tableau de 2 enregistrements représentant des joueurs, de la forme :
+       {
+          x: position de départ en x du joueur
+          y: position de départ en y du joueur
+          id: numéro d'identification du joueur
+          direction: direction initiale du joueur
+       }
+     me: numéro d'identification (id) de votre joueur
+   }
+ */
+function createGrid(config) {
     Grid.create(config.h, config.w);
 
     // TODO : initialiser les différentes variables globales représentant l'état du jeu
